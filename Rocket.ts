8468a65cbebc5905cc775d7ec13a58ff82154857
miccraig15 <miccraig15@gmail.com>
2022-12-15 16:52:44 -0600

    Added astronaut, cargo, and rocket classes

--- Rocket.ts
+++ Rocket.ts
@@ -1,11 +1,22 @@
+import{Payload} from './Payload';
+import { Astronaut } from './Astronaut';
+import { Cargo } from './Cargo';
+
 export class Rocket {
     name: string;
     totalCapacityKg: number;
-    cargoItems = [];
-    astronauts = [];
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
     constructor(name: string, totalCapacityKg: number) {
         this.name = name;
         this.totalCapacityKg = totalCapacityKg;
+    }
+    sumMass(items: Payload[]): number {
+        let sum: number = 0
+            for (let i = 0; i < items.length; i++) {
+               sum += items[i].massKg;
+            }
+            return sum;
     }
     
 }
