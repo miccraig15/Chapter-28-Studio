021ae5185ed179932bad70082e704bfc0ba91b85
miccraig15 <miccraig15@gmail.com>
2023-01-01 03:31:22 -0600

    Finish Rocket Class

--- Rocket.ts
+++ Rocket.ts
@@ -18,5 +18,30 @@ export class Rocket {
             }
             return sum;
     }
-    
+    currentMassKg(): number {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    }
+    canAdd(item: Payload): boolean {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
